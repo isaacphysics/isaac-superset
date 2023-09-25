cf32b9d8c015e2734ae0fb8cc7828c2d6324955f
Matthew Trew <matthew.trew@raspberrypi.org>
2023-09-25 17:20:02 +0100

    Correct restore script, remove static IP

--- superset/restore-postgres.sh
+++ superset/restore-postgres.sh
@@ -1,3 +1,3 @@
 file_to_restore=$1
 
-gunzip -c "$file_to_restore" | docker exec -i superset_db pg_restore -U superset -f -
+gunzip -c "$file_to_restore" | docker exec -i superset_db psql -U superset -f -
